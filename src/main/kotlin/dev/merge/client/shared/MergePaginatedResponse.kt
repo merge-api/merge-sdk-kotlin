f69745a3eaa75e58e411b90eccf504e86dac143b
leewang0 <lee@merge.dev>
2022-08-11 12:52:11 -0400

    add shared pagination and remote data classes

---
+++ src/main/kotlin/dev/merge/client/shared/MergePaginatedResponse.kt
@@ -0,0 +1,41 @@
+/**
+ * Merge API
+ *
+ * The unified API for building rich integrations with multiple Accounting & Finance platforms.
+ *
+ * The version of the OpenAPI document: 1.0
+ * Contact: hello@merge.dev
+ */
+
+@file:Suppress(
+    "ArrayInDataClass",
+    "EnumEntryName",
+    "RemoveRedundantQualifierName",
+    "UnusedImport"
+)
+
+package dev.merge.client.shared
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+/**
+ * 
+ *
+ * @param next 
+ * @param previous 
+ * @param results 
+ */
+
+data class MergePaginatedResponse<T> (
+
+    @field:JsonProperty("next")
+    val next: kotlin.String? = null,
+
+    @field:JsonProperty("previous")
+    val previous: kotlin.String? = null,
+
+    @field:JsonProperty("results")
+    val results: kotlin.collections.List<T>? = null
+
+)
+
