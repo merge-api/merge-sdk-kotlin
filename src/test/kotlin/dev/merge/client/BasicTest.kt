da691154331ce53a1ceeca6189a5ced5e1ca3f27
leewang0 <lee@merge.dev>
2022-08-16 11:43:08 -0400

    add test plumbing

---
+++ src/test/kotlin/dev/merge/client/BasicTest.kt
@@ -0,0 +1,12 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class BasicTest {
+    @Test
+    fun testAllCategoriesSimple() {
+        // val expected = 42
+        // assertEquals(expected, testSample.sum(40, 2))
+
+        assertEquals(true, true)
+    }
+}
